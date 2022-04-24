c3eeaef5bf95f202e2a40a5f72e265eacf86c8a1
sasangka lambang k <94464357+salak96@users.noreply.github.com>
2022-04-24 15:51:11 +0700

    auth logout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -62,14 +62,7 @@
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="/" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="/" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
-                                    </form>
-                                    <hr>
+                                        
                                     <div class="text-center">
                                         <a class="small" href="/forgot-password">Forgot Password?</a>
                                     </div>
