700f98751c8fd813e29d6ba5bc313ca787cb9c73
sasangka lambang k <94464357+salak96@users.noreply.github.com>
2022-05-19 19:53:28 +0700

    bikin dashboard kasir cek Level

--- app/Http/Middleware/Ceklevel.php
+++ app/Http/Middleware/Ceklevel.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class Ceklevel
+class CekLevel
 {
     /**
      * Handle an incoming request.
@@ -14,11 +14,12 @@ class Ceklevel
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next,...$levels)
+    public function handle(Request $request, Closure $next, ...$level)
     {
-      if (in_array($request->user()->level, $levels)) {
-        return $next($request);
-      }
-      return redirect('/');
+        if (auth()->user() && in_array(auth()->user()->level, $level)) {
+            return $next($request);
+        }
+
+        return redirect()->route('dashboard');
     }
 }
